d9b2076ef0b554c4d50867769a82b6e223335a66
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-21 15:31:16 +0530

    sidebar-loading-error-page

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import loader from '../assets/loader.svg';
 
-const Loader = () => {
+const Loader = ({ title }) => {
   return (
-    <div>Loader</div>
+    <div className='w-full flex justify-center items-center flex-col'>
+      <img src={loader} alt='loader' className='w-32 h-32 object-contain' />
+
+      <h1 className='font-bold text-2xl text-white mt-2'>{ title || "Loading......"}</h1>
+    </div>
   )
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import sidebarLogo from "../assets/sidebar.png";
+import { NavLink } from "react-router-dom";
+import {
+  HiOutlineHashtag,
+  HiOutlineHome,
+  HiOutlineMenu,
+  HiOutlinePhotograph,
+  HiOutlineUserGroup,
+} from "react-icons/hi";
+import { RiCloseLine } from "react-icons/ri";
+
+const links = [
+  { name: "Discover", to: "/", icon: HiOutlineHome },
+  { name: "Around You", to: "/around-you", icon: HiOutlinePhotograph },
+  { name: "Top Artists", to: "/top-artists", icon: HiOutlineUserGroup },
+  { name: "Top Charts", to: "/top-charts", icon: HiOutlineHashtag },
+];
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-export default Sidebar
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={sidebarLogo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div> 
+
+
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine  className="2-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)}/>
+        ): (
+          <HiOutlineMenu  className="2-6 h-6 text-white mr-2" onClick={() =>setMobileMenuOpen(true)}/>
+        )}
+      </div>
+
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483e8b] backdrop:-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={sidebarLogo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() =>setMobileMenuOpen} />
+      </div> 
+    </>
+  );
+};
+
+export default Sidebar;
