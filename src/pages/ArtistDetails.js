ce8693d845ef557108269b6435ea4f4cdc1bd24c
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-22 19:54:59 +0530

    relatedsong-discover-song-details--

--- src/pages/ArtistDetails.js
+++ src/pages/ArtistDetails.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import DetailsHeader from '../components/DetailsHeader';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import Error from '../components/Error';
+import Loader from '../components/Loader';
+import RelatedSongs from "../components/RelatedSongs";
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+
 
 const ArtistDetails = () => {
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
+
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
+
+  if (error) return <Error />;
+
   return (
-    <div>ArtistDetails</div>
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData?.data[0]}
+      />
+
+      <RelatedSongs
+        data={artistData?.data[0].views['top-songs']?.data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
   )
 }
 
